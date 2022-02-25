0084263e95eb7d121edbb9617a354ce17d2307dd
angular403 <angularsprings403@gmail.com>
2022-02-26 03:26:50 +0700

    finish

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:latihanapicli/app/data/models/user_model.dart';
 
 import '../controllers/home_controller.dart';
 
@@ -12,12 +13,36 @@ class HomeView extends GetView<HomeController> {
         title: Text('HomeView latihan Cli'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
+      body: FutureBuilder<List<User>>(
+          future: controller.getAllUsers(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(
+                  color: Colors.red,
+                ),
+              );
+            }
+            if (snapshot.data?.length == 0) {
+              return Center(
+                child: Text("Data Tidak ada"),
+              );
+            } else {
+              return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) {
+                  User user = snapshot.data![index];
+                  return ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage("${user.avatar}"),
+                    ),
+                    title: Text("${user.firstName} ${user.lastName}"),
+                    subtitle: Text("${user.email}"),
+                  );
+                },
+              );
+            }
+          }),
     );
   }
 }
